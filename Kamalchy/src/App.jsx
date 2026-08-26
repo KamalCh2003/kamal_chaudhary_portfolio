68091a73a5a2b7842209fe04c0dd17528b96e889
Kamal Chaudhary <kamalchy110@gmail.com>
2026-08-26 19:45:53 +0545

    Update application structure and components

--- Kamalchy/src/App.jsx
+++ Kamalchy/src/App.jsx
@@ -2,6 +2,7 @@ import { Navbar } from './components/layout/Navbar';
 import { Footer } from './components/layout/Footer';
 import { Hero } from './components/sections/Hero';
 import { About } from './components/sections/About';
+import { Skills } from './components/sections/Skills';
 import { Projects } from './components/sections/Projects';
 import { Experience } from './components/sections/Experience';
 import { Contact } from './components/sections/Contact';
@@ -25,9 +26,10 @@ function App() {
         rippleDuration={600}
       />
       <Navbar />
-      <main className="relative z-10">
+      <main className="relative z-10 px-2 sm:px-20 md:px-20">
         <Hero />
         <About />
+        <Skills />
         <Projects />
         <Experience />
         <Contact />
